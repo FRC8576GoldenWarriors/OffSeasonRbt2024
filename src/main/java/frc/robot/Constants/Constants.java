f2af2ab880668646db9427e19862b34219cc57d8
GWR Programming <goldenwarriorsrobotics@gmail.com>
2024-12-05 20:40:48 -0600

    commented code to fix

--- src/main/java/frc/robot/Constants/Constants.java
+++ src/main/java/frc/robot/Constants/Constants.java
@@ -40,26 +40,8 @@ public class Constants {
     }
 
     public static final class SwerveConstants{
-        //Drivetrain motor/encoder IDs
-    
-        //PRACTICE IDS
-        // public static final int LEFT_FRONT_DRIVE_ID = 7;
-        // public static final int RIGHT_FRONT_DRIVE_ID = 1;
-        // public static  final int LEFT_BACK_DRIVE_ID = 4;
-        // public static final int RIGHT_BACK_DRIVE_ID = 3;
-        
-        // public static final int LEFT_FRONT_TURN_ID = 6;
-        // public static final int RIGHT_FRONT_TURN_ID = 8;
-        // public static  final int LEFT_BACK_TURN_ID = 5; 
-        // public static final int RIGHT_BACK_TURN_ID = 2;
-    
-        // public static final int LEFT_FRONT_CANCODER_ID = 2;
-        // public static final int RIGHT_FRONT_CANCODER_ID = 4;
-        // public static final int LEFT_BACK_CANCODER_ID = 1;
-        // public static final int RIGHT_BACK_CANCODER_ID = 3;
-    
-        //  public static final int PIGEON_ID = 0;
-    
+
+  //INPUT IDS NEED TO BE CHECKED
     
         //COMPETITION IDS
     
@@ -80,16 +62,9 @@ public class Constants {
     
         public static final int PIGEON_ID = 0;
     
-        //Drivetrain characteristics
-    
-        //PRACTICE OFFSETS
-        // public static  double LEFT_FRONT_OFFSET = 0.004150;
-        // public static  double RIGHT_FRONT_OFFSET = 0.907227;
-        // public static  double LEFT_BACK_OFFSET = 0.571533;
-        // public static  double RIGHT_BACK_OFFSET = 0.731201;
     
         //COMP OFFSETS
-        public static  double LEFT_FRONT_OFFSET =0.205811;//0.206055; //0.222412;//0.213135;//0.216064;//0.721680;//0.732666;//0.292969;//0.723145;//0.717041;//0.717041;//0.710938 ;//0.725098;//0.719971;	
+        public static  double LEFT_FRONT_OFFSET =0.205811;
         public static  double RIGHT_FRONT_OFFSET = 0.798828;//0.301514;//0.799072;//0.302246;//0.306885;//0.792725;//0.712891;//0.817383;
         public static  double LEFT_BACK_OFFSET = 0.5217290;//0.524902;//0.017334;//0.794189;//0.818115;//0.300781;//0.303711;//0.847412;//0.448730;//0.455322;//0.418945;//0.490967;//0.519043;
         public static  double RIGHT_BACK_OFFSET = 0.666748;//0.514160;//0.512451;//0.011475 ;//0.012451;//0.014404;//0.510498;//0.504883;//0.013916; 
@@ -128,6 +103,7 @@ public class Constants {
         );
     
     
+        //CREATE NEW CONSTANTS FOR LENGTH AND WIDTH
         //Swerve Kinematics
         public static final SwerveDriveKinematics DRIVE_KINEMATICS = new SwerveDriveKinematics(
             new Translation2d(WHEEL_BASE / 2, TRACK_WIDTH / 2),
